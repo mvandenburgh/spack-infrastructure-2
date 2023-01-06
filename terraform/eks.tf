61beda52d4ff9ac21d26299f7c2d23e65326da0b
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2023-01-06 17:05:38 -0500

    Add terraform for EKS cluster

---
+++ terraform/eks.tf
@@ -0,0 +1,65 @@
+module "eks" {
+  # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.31.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.24"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Enables OIDC provider for cluster; required for Karpenter
+  enable_irsa = true
+
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+    ingress_nodes_karpenter_port = {
+      description                   = "Cluster API to Node group for Karpenter webhook"
+      protocol                      = "tcp"
+      from_port                     = 8443
+      to_port                       = 8443
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
+
+  node_security_group_tags = {
+    # NOTE - if creating multiple security groups with this module, only tag the
+    # security group that Karpenter should utilize with the following tag
+    # (i.e. - at most, only one security group should have this tag in your account)
+    "karpenter.sh/discovery" = local.cluster_name
+  }
+
+  # Only need one node to get Karpenter up and running.
+  # This ensures core services such as VPC CNI, CoreDNS, etc. are up and running
+  # so that Karpenter can be deployed and start managing compute capacity as required
+  eks_managed_node_groups = {
+    initial = {
+      instance_types = ["m5.large"]
+      # Not required nor used - avoid tagging two security groups with same tag as well
+      create_security_group = false
+
+      # Ensure enough capacity to run 2 Karpenter pods
+      min_size     = 2
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+}
